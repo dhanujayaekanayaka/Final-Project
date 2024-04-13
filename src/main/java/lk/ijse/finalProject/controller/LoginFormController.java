1bc3862cc9da8da4ed247592aa1ddc0dc9876ba5
dhanujaya <sahandanujaya48@gmail.com>
2024-04-13 15:52:23 +0530

    git push

---
+++ src/main/java/lk/ijse/finalProject/controller/LoginFormController.java
@@ -0,0 +1,24 @@
+package lk.ijse.finalProject.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+    public TextField txtUsername;
+    public TextField txtPassword;
+
+    public void txtUsernameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
+
+    public void hplForgotPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void hplRegisterOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/lk/ijse/finalProject/controller/RegisterFormController.java
@@ -0,0 +1,25 @@
+package lk.ijse.finalProject.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class RegisterFormController {
+    public TextField txtUsername;
+    public TextField txtPassword;
+    public TextField txtReEnterPassword;
+
+    public void txtUsernameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtReEnterPasswordOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+}
