6b3b8f1a15a7d9cc2cd34ec88582ef1f5943fd96
dhanujaya <sahandanujaya48@gmail.com>
2024-04-13 14:53:56 +0530

    git push

---
+++ src/main/java/lk/ijse/finalProject/Launcher.java
@@ -0,0 +1,15 @@
+package lk.ijse.finalProject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
