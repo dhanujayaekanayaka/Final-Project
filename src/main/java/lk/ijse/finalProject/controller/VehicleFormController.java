dfe846d90c8c1ee22010aecec10ad8115f928ec4
dhanujaya <sahandanujaya48@gmail.com>
2024-04-21 10:31:53 +0530

    git push

--- src/main/java/lk/ijse/finalProject/controller/VehicleFormController.java
+++ src/main/java/lk/ijse/finalProject/controller/VehicleFormController.java
@@ -1,4 +1,26 @@
 package lk.ijse.finalProject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class VehicleFormController {
+    public TextField txtModel;
+    public TextField txtVehicleNumber;
+    public TextField txtColor;
+    public TextField txtYom;
+    public TextField txtRegDate;
+    public TextField txtCurrentMillage;
+    public TextField txtChassisNumber;
+    public TextField txtEngineNumber;
+    public ImageView imvProfilePicture;
+
+    public void btnConformOnAction(ActionEvent event) {
+    }
+
+    public void btnClearOnAction(ActionEvent event) {
+    }
+
+    public void btnSaveOnAction(ActionEvent event) {
+    }
 }
